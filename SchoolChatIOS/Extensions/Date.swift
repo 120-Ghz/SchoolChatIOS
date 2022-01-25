e251b9b1d7b68e82c12f0b550ab89f5d87d5a677
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-01-25 13:15:51 +0300

    Updating Date and adding Unit tests

--- SchoolChatIOS/Extensions/Date.swift
+++ SchoolChatIOS/Extensions/Date.swift
@@ -8,51 +8,51 @@
 import Foundation
 
 extension Date {
+    func DayFromNumber(num: Int) -> String {
+        switch (num) {
+        case 1:
+            return "Sun"
+        case 2:
+            return "Mon"
+        case 3:
+            return "Tue"
+        case 4:
+            return "Wed"
+        case 5:
+            return "Thu"
+        case 6:
+            return "Fri"
+        case 7:
+            return "Sat"
+        default:
+            return "Day"
+        }
+    }
+    
+    func MonthFromNumber(num: Int) -> String {
+        switch (num) {
+        case 1, 2, 3, 4, 5, 6, 7, 8, 9:
+            return "0\(num)"
+        case 10, 11, 12:
+            return String(num)
+        default:
+            return ""
+        }
+    }
+    
+    func TimeFormat(hour: Int, minutes: Int) -> String {
+        var hoursstr = String(hour)
+        var minutestr = String(minutes)
+        if (hour < 10) {
+            hoursstr = "0" + hoursstr
+        }
+        if (minutes < 10) {
+            minutestr = "0" + minutestr
+        }
+        return hoursstr + ":" + minutestr
+    }
+    
     func FormatDateToChatRow() -> String {
-        
-        func DayFromNumber(num: Int) -> String {
-            switch (num) {
-            case 1:
-                return "Sun"
-            case 2:
-                return "Mon"
-            case 3:
-                return "Tue"
-            case 4:
-                return "Wed"
-            case 5:
-                return "Thu"
-            case 6:
-                return "Fri"
-            case 7:
-                return "Sat"
-            default:
-                return "Day"
-            }
-        }
-        
-        func MonthFromNumber(num: Int) -> String {
-            switch (num) {
-            case 1, 2, 3, 4, 5, 6, 7, 8, 9:
-                return "0\(num)"
-            case 10, 11, 12:
-                return String(num)
-            default:
-                return ""
-            }
-        }
-        
-        func TimeFormat(hour: Int, minutes: Int) -> String {
-            var hoursstr = String(hour)
-            var minutestr = String(minutes)
-            if (hour < 10) {
-                hoursstr = "0" + hoursstr
-            }
-            if (minutes < 10) {
-                minutestr = "0" + minutestr
-            }
-            return hoursstr + ":" + minutestr
-        }
         
         let calendar = Calendar.current
         
--- SchoolChatIOSTests/SchoolChatIOSTests.swift
+++ SchoolChatIOSTests/SchoolChatIOSTests.swift
@@ -17,17 +17,38 @@ class SchoolChatIOSTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testDayFromNumber() throws {
+//        var components = DateComponents()
+//        components.year = 2022
+//        components.month = 1
+//        components.day = 24
+//        let calendar = Calendar(identifier: .gregorian)
+//        let date = calendar.date(from: components)
+        XCTAssertEqual(Date().DayFromNumber(num: 3), "Tue")
+    }
+    
+    func testMonthFromNumber1DigitInput() throws {
+        XCTAssertEqual(Date().MonthFromNumber(num: 4), "04")
+    }
+    
+    func testMonthFromNumber2DigitInput() throws {
+        XCTAssertEqual(Date().MonthFromNumber(num: 12), "12")
+    }
+    
+    func testTimeFormat1DigitInputs() throws {
+        XCTAssertEqual(Date().TimeFormat(hour: 1, minutes: 6), "01:06")
+    }
+    
+    func testTimeFormat2DigitInputs() throws {
+        XCTAssertEqual(Date().TimeFormat(hour: 11, minutes: 46), "11:46")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
