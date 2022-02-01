c5abff970503ab2dbeed9e2a1ebf07330f0e5dd8
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-02-01 23:03:54 +0300

    upd

---
+++ SchoolChatIOS/Extensions/Other.swift
@@ -0,0 +1,25 @@
+//
+//  Other.swift
+//  SchoolChatIOS
+//
+//  Created by Константин Леонов on 01.02.2022.
+//
+
+import Foundation
+
+func pow<T: BinaryInteger>(_ base: T, _ power: T) -> T {
+    func expBySq(_ y: T, _ x: T, _ n: T) -> T {
+        precondition(n >= 0)
+        if n == 0 {
+            return y
+        } else if n == 1 {
+            return y * x
+        } else if n.isMultiple(of: 2) {
+            return expBySq(y, x * x, n / 2)
+        } else { // n is odd
+            return expBySq(y * x, x * x, (n - 1) / 2)
+        }
+    }
+
+    return expBySq(1, base, power)
+}
--- SchoolChatIOS/Views/Auth/AuthView.swift
+++ SchoolChatIOS/Views/Auth/AuthView.swift
@@ -16,49 +16,60 @@ struct AuthView: View {
     var TextColor = Color(red: 90/255, green: 0, blue: 90/255)
     let screenWidth = UIScreen.main.bounds.size.width
     
-    
+    var defaultHeight = 844.0
+    var defaultWidth = 390.0
     
     var body: some View {
-        ZStack {
+        VStack {
             VStack {
-                Spacer()
-                Text(SignUp ? "Регистрация" : "Авторизация")
-                    .font(Font.custom("helvetica", size: 30))
-                    .fontWeight(.semibold)
-                    .foregroundColor(TextColor)
-                    .padding(.bottom, 30)
-                    .padding(.top, 50)
-                Spacer()
-                if SignUp {
-                    SignUpView(AuthOb: AuthOb)
-                } else {
-                    SignInView(AuthO: AuthOb)
-                }
-                Spacer()
-                Spacer()
-                HStack {
-                    Text("Авторизация")
-                        .fontWeight(.semibold)
-                        .foregroundColor(SignUp ? HintColor : .white)
+                ZStack {
+                    VStack {
+                        Spacer()
+                        Text(SignUp ? "Регистрация" : "Авторизация")
+                            .font(Font.custom("helvetica", size: 30))
+                            .fontWeight(.semibold)
+                            .foregroundColor(TextColor)
+                            .padding()
+                            
+                        Spacer()
+                        
+                        if SignUp {
+                            SignUpView(AuthOb: AuthOb)
+                        } else {
+                            SignInView(AuthO: AuthOb)
+                        }
+                        Spacer()
+                        Spacer()
+                        HStack {
+                            Text("Авторизация")
+                                .fontWeight(.semibold)
+                                .foregroundColor(SignUp ? HintColor : .white)
+                                .padding()
+                                .background(Capsule().fill(SignUp ? Color.white : Color.purple.opacity(SignUp ? 0 : 1)).frame(height: 50))
+                                .shadow(color: Color(UIColor(Color.purple).withAlphaComponent(SignUp ? 0 : 0.8)), radius: 8, x: 0, y: 9)
+                            Text("Регистрация")
+                                .fontWeight(.semibold)
+                                .foregroundColor(SignUp ? .white : HintColor)
+                                .padding()
+                                .background(Capsule().fill( SignUp ?  Color.purple.opacity(SignUp ? 1 : 0) : Color.white).frame(height: 50))
+                                .shadow(color: Color(UIColor(Color.purple).withAlphaComponent(SignUp ? 0.8 : 0)), radius: 8, x: 0, y: 9)
+                            
+                        }
+                        .onTapGesture {
+                            withAnimation {
+                                SignUp.toggle()
+                            }
+                        }
                         .padding()
-                        .background(Capsule().fill(Color.purple.opacity(SignUp ? 0 : 1)).frame(height: 50))
-                        .shadow(color: Color(UIColor(Color.purple).withAlphaComponent(SignUp ? 0 : 0.8)), radius: 8, x: 0, y: 9)
-                    Text("Регистрация")
-                        .fontWeight(.semibold)
-                        .foregroundColor(SignUp ? .white : HintColor)
-                        .padding()
-                        .background(Capsule().fill(Color.purple.opacity(SignUp ? 1 : 0)).frame(height: 50))
-                        .shadow(color: Color(UIColor(Color.purple).withAlphaComponent(SignUp ? 0.8 : 0)), radius: 8, x: 0, y: 9)
-                    
-                }
-                .onTapGesture {
-                    withAnimation {
-                        SignUp.toggle()
+                        .padding(.top, 5)
                     }
                 }
-                .padding()
-                .padding(.top, 5)
             }
+            .padding(.bottom, 4)
+            .scaleEffect( UIScreen.main.bounds.height < defaultHeight ? UIScreen.main.bounds.height/(defaultHeight*0.9) : 1)
+            .onAppear(perform: {
+                print(UIScreen.main.bounds.width)
+            })
         }
         .background(LinearGradient(gradient: Gradient(colors: [.white, .purple.opacity(0.2), .yellow.opacity(0.7)]), startPoint: .topTrailing, endPoint: .bottomLeading))
     }
